8e1f6607fc9343d9df41e4c2473b865b82392413
Masoud Andalibi <mas_iust@yahoo.com>
2023-09-10 13:37:13 +0330

    #4 - adds patient aggregate

---
+++ src/Example.Domain/Aggregates/PatientAggregate/PatientCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Domain.Entities;
+
+namespace Example.Domain.Aggregates.PatientAggregate;
+public class PatientCategory : Entity<Guid>
+{
+    public PatientCategory(Guid patientCategoryId, Guid categoryId, Guid patientId)
+    {
+        Id = patientCategoryId;
+        CategoryId = categoryId;
+        PatientId = patientId;
+    }
+
+    public Guid CategoryId { get; protected set; }
+    public Guid PatientId { get; protected set; }
+}
---
+++ src/Example.Domain/Aggregates/PatientAggregate/PatientFileStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Volo.Abp.Domain.Entities;
+
+namespace Example.Domain.Aggregates.PatientAggregate;
+public class PatientFileStore : Entity<Guid>
+{
+    public PatientFileStore(Guid patientFileStoreId, Guid patientId, Guid fileId)
+    {
+        Id = patientFileStoreId;
+        PatientId = patientId;
+        FileId = fileId;
+    }
+
+    public Guid PatientId { get; protected set; }
+    public Guid FileId { get; protected set; }
+
+}
